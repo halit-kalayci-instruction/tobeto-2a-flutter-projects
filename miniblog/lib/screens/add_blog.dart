a9cb31aea30ec3e31ff3c9fa262b6c52381bf302
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-04-04 10:47:08 +0300

    http & filepicker

--- miniblog/lib/screens/add_blog.dart
+++ miniblog/lib/screens/add_blog.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:image_picker/image_picker.dart';
 
 class AddBlog extends StatefulWidget {
   const AddBlog({Key? key}) : super(key: key);
@@ -11,10 +15,38 @@ class _AddBlogState extends State<AddBlog> {
   final _formKey = GlobalKey<FormState>();
   String blogTitle = "";
   String blogContent = "";
+  XFile? selectedImage;
+  String author = "";
 
-  void submit() {
-    print(blogTitle);
-    print(blogContent);
+  void _submit() async {
+    if (selectedImage != null) {
+      Uri url = Uri.parse("https://tobetoapi.halitkalayci.com/api/Articles");
+
+      var request = http.MultipartRequest("POST", url);
+      request.fields["Title"] = blogTitle;
+      request.fields["Content"] = blogContent;
+      request.fields["Author"] = author;
+
+      final file =
+          await http.MultipartFile.fromPath("File", selectedImage!.path);
+      request.files.add(file);
+
+      final response = await request.send();
+
+      if (response.statusCode == 201) {
+        // Başarılı
+        Navigator.pop(context);
+      }
+    }
+  }
+
+  void _pickImage() async {
+    final imagePicker = ImagePicker();
+    XFile? file = await imagePicker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      selectedImage = file;
+    });
   }
 
   @override
@@ -28,6 +60,16 @@ class _AddBlogState extends State<AddBlog> {
           child: Form(
             key: _formKey,
             child: Column(children: [
+              if (selectedImage != null)
+                Image.file(
+                  File(selectedImage!.path),
+                  height: 250,
+                ),
+              ElevatedButton(
+                  onPressed: () {
+                    _pickImage();
+                  },
+                  child: const Text("Fotoğraf Seç")),
               TextFormField(
                 decoration: const InputDecoration(
                   label: Text("Blog Başlığı Giriniz"),
@@ -56,11 +98,25 @@ class _AddBlogState extends State<AddBlog> {
                   return null;
                 },
               ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  label: Text("Yazar İsmi Giriniz"),
+                ),
+                onSaved: (newValue) {
+                  author = newValue!;
+                },
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Yazar ismi boş olamaz.";
+                  }
+                  return null;
+                },
+              ),
               ElevatedButton(
                   onPressed: () {
                     if (_formKey.currentState!.validate()) {
                       _formKey.currentState!.save();
-                      submit();
+                      _submit();
                     }
                   },
                   child: const Text("Kaydet"))
@@ -69,3 +125,4 @@ class _AddBlogState extends State<AddBlog> {
         ));
   }
 }
+// 10:05
